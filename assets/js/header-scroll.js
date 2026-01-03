9829fc9718bb67e811fb17dc951dd36e176de36e
Staffan Canback <71180109+scanback@users.noreply.github.com>
2026-01-03 19:34:13 +0100

    Update header-scroll.js

--- assets/js/header-scroll.js
+++ assets/js/header-scroll.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", () => {
     const currentScrollY = window.scrollY;
 
-    if (currentScrollY > 80 && currentScrollY > lastScrollY) {
+    if (currentScrollY > 150 && currentScrollY > lastScrollY) {
       // scrolling down
       header.classList.add("is-hidden");
     } else {
